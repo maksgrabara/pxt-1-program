d68e4819645d514d4979a3b2640d8447de08102a
Maksymilian Grabara <56476950+maksgrabara@users.noreply.github.com>
2020-10-03 09:23:19 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
 basic.showString("Hello!")
+basic.clearScreen()
 basic.forever(function () {
     for (let index = 0; index < 4; index++) {
-    	
-        basic.showNumber(0)
-
         basic.pause(100)
+        music.ringTone(262)
     }
 })
